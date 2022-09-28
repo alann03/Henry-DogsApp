b50538aeeee461403611d8f9689e9d233c0def69
alann03 <alanhuismann03@gmail.com>
2022-09-28 20:40:10 -0300

    config env

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-	server.listen(3001, () => {
-		console.log("%s listening at 3001");
+	server.listen(PORT, () => {
+		console.log("%s listening");
 	});
 });
